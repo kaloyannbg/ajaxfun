8594eee0624944e2180cc7f31a90d735e9a7bdb4
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-08-05 03:11:44 +0300

    Update real_time_chat.php

--- real_time_chat.php
+++ real_time_chat.php
@@ -5,5 +5,5 @@ include 'config.php';
 
 $query = query("SELECT * FROM messages");
 while ($row = mysqli_fetch_assoc($query)) {
-    echo '<p>[ '.date("m/d/y, H:s:i", $row['time']) . ' ] ' .$row['message'] . '</p>';
+    echo '<p>[ '.date("m/d/y, H:i:s", $row['time']) . ' ] ' .$row['message'] . '</p>';
 }
